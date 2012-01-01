d912b8aecf4f345036a71b8e9e3ae7b3c33fd038
Mickael Daniel <daniel.mickael@gmail.com>
2012-01-01 15:46:17 +0100

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+
+# simple deploy script
+
+# first checkout the correct branch, no matter if we're already on it
+git checkout gh-pages
+
+# optionnally, merge in any changes from master
+git merge master
+
+# if ok, then push on the live site
+# git push origin gh-pages
+
+echo "push it push it"
