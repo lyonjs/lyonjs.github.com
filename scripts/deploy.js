2d68f3cfbd7ca2e2b871964fee50e8fba2973a5b
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-08 10:55:30 +0100

    deploy - simpler deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,35 +8,28 @@ task.registerBasicTask('deploy', 'Simple deploy script, prepares the gh-pages br
   var cb = this.async(),
     msg = 'build: generates site content (:date)'.replace(':date', new Date);
 
-  // first, let's start our gh-pages branch, with no ancestor
-  exec('git symbolic-ref HEAD refs/heads/' + name, function(err, stdout, stderr) {
+  // then, generate site's content
+  // first, let's start a new build
+  task.helper('bilbo', data, function(err) {
     if(err) return fail.warn(err, 3);
 
-    log.writeln(stdout);
+    // then, switch to appropriate branch. add the necessary files, forcing the
+    // add of out/ (.gitignore'd) while cleaning out non tracked files (but
+    // make sure to keep node_modules there). Finally, move any files under
+    // out/* to the repo's root and commit.
+    var cmds = [
+      'git checkout ' + name,
+      'git add out/ .gitignore CNAME favicon.ico -f',
+      'git clean -fdx -e node_modules',
+      'git mv out/* .',
+      'git commit -m "' + msg + '"'
+    ].join(' && ');
 
-    // then, generate site's content
-    task.helper('bilbo', data, function(err) {
+    exec(cmds, function(err, stdout) {
       if(err) return fail.warn(err, 3);
-
-      // then, add the necessary files, forcing the add of out/
-      // (.gitignore'd) while cleaning out non tracked files
-      // (but make sure to keep node_modules there). Finally,
-      // move any files under out/* to the repo's root and commit.
-      var cmds = [
-        'git add out/ .gitignore CNAME favicon.ico -f',
-        'git clean -fdx -e node_modules',
-        'git mv out/* .',
-        'git commit -m "' + msg + '"'
-      ].join(' && ');
-
-      exec(cmds, function(err, stdout) {
-        if(err) return fail.warn(err, 3);
-        log.writeln(stdout);
-        cb();
-      });
-
+      log.writeln(stdout);
+      cb();
     });
-
   });
 
 });
