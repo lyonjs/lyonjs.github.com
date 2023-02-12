a8f10c4f7d3b9875f23422accae44fd752f5de2e
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-02-12 14:13:11 +0100

    feat(seo): add meta tags on pages

--- modules/header/LyonJSHead.tsx
+++ modules/header/LyonJSHead.tsx
@@ -1,9 +1,27 @@
 import Head from 'next/head';
 
+const title = 'LyonJS - Communauté lyonnaise des utilisateurs de JavaScript'
+const description = 'Communauté lyonnaise des utilisateurs de JavaScript, vous retrouverez ici le replay de nos derniers meetups ainsi que les dates des prochains évènements, conférences, soirées, partage, connaissance, talk, speakers';
+const url = 'http://lyonjs.org'
+const image = `${url}/lyonjs.webp`
 export const LyonJSHead = () => (
   <Head>
-    <title>LyonJS - Communauté lyonnaise des utilisateurs de JavaScript</title>
-    <meta name="description" content="Communauté lyonnaise des utilisateurs de JavaScript" />
+    <title>{title}</title>
+    <meta name="title" content={title}/>
+    <meta name="description" content={description} />
+
+    <meta property="og:url" content={url}/>
+    <meta property="og:type" content="website"/>
+    <meta name="og:title" content={title}/>
+    <meta name="og:description" content={description}/>
+    <meta name="og:image" content={image}/>
+
+    <meta property="twitter:card" content="summary_large_image"/>
+    <meta property="twitter:url" content={url}/>
+    <meta property="twitter:title" content={title}/>
+    <meta property="twitter:description" content={description} />
+    <meta property="twitter:image" content={image} />
+
     <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
