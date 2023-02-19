013c5dce2fd1be377deb576d64faf26c72e2c6ac
Mathieu Mure <mathieu.mure@gmail.com>
2023-02-19 18:57:41 +0100

    fix: export mode is not compatbile with optimised images

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
   distDir: 'dist',
   images: {
     domains: ['pbs.twimg.com'],
+    unoptimized: true, // disable as we use export mode (which is not compatible see https://nextjs.org/docs/messages/export-image-api)
   },
 };
 
