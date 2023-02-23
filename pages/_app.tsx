6173339713d47c6cd5cedebfac382aef2c2cdc86
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-02-23 21:43:32 +0100

    refactor(next13.2): migrate to mdx native support of next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,29 +4,16 @@ import 'dayjs/locale/fr';
 import dayjs from 'dayjs';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { H1, H2 } from '../modules/atoms/remark/titles';
-import { A, Li, P, Ul } from '../modules/atoms/remark/text';
-import { ResponsiveImage } from '../modules/atoms/remark/image';
 import { Header } from '../modules/header/Header';
 import { Footer } from '../modules/Footer';
 
 dayjs.locale('fr');
 
-const components = {
-  img: ResponsiveImage,
-  h1: H1,
-  h2: H2,
-  p: P,
-  ul: Ul,
-  li: Li,
-  a: A,
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} components={components} />
+      <Component {...pageProps} />
       <Footer />
     </>
   );
