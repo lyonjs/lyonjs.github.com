340d255b13062cb73ce083ac8910d8d30db921c0
Antoine Caron <Slashgear@users.noreply.github.com>
2023-03-02 08:51:02 +0100

    feat: add past event page (#64)

---
+++ modules/event/YearNaviation.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './year-navigation.module.css';
+import { ActiveLink } from './ActiveLink';
+
+export const YearNavigation: React.FC<{ years: string[] }> = ({ years }) => {
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.list}>
+        {years.map<React.ReactNode>((year) => (
+          <li key={year}>
+            <ActiveLink href={`/evenements-precedents/${year}`}>{year}</ActiveLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
