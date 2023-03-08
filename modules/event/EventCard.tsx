1c7ad14a6a58a86d01731fe7a5b12b319504aee8
Julien Sulpis <julien.sulpis@gmail.com>
2023-03-08 22:31:12 +0100

    feat(home): fix the next event on mobile

--- modules/event/EventCard.tsx
+++ modules/event/EventCard.tsx
@@ -10,61 +10,75 @@ import styles from './EventCard.module.css';
 import { Calendar } from '../icons/Calendar';
 import { Meetup } from '../icons/Meetup';
 import { EventMarkup } from './EventMarkup';
+import classNames from 'classnames';
 
-export const EventCard: FC<{ event: Event }> = ({ event }) => {
+type EventProps = { event: Event };
+
+const Location: FC<EventProps> = ({ event }) => (
+  <a
+    href={`http://maps.google.com/maps?q=loc:${event.venue.lat}+${event.venue.lng}`}
+    target="_blank"
+    rel="noreferrer noopener"
+    className={styles.location}
+  >
+    <Pin />
+    <div>
+      {event.sponsor && <p className={styles.sponsor}>{event.sponsor.name}</p>}
+      <span>{event.venue.address},</span>
+      <span className={styles.city}>
+        {event.venue.postalCode} {event.venue.city}
+      </span>
+    </div>
+  </a>
+);
+
+const Description: FC<EventProps> = ({ event }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <div
+        className={classNames(styles.description, {
+          [styles.expanded]: isExpanded,
+        })}
+      >
+        <ReactMarkdown>{event.description}</ReactMarkdown>
+      </div>
+      <button type="button" className={styles.seeMore} onClick={() => setIsExpanded((isExpanded) => !isExpanded)}>
+        {isExpanded ? 'Voir moins' : 'Voir plus'}
+      </button>
+    </>
+  );
+};
+
+export const EventCard: FC<EventProps> = ({ event }) => {
   const dateParsed = dayjs(event.dateTime);
   const formattedDayAndMonth = _capitalize(dateParsed.format('dddd D MMMM YYYY à H:mm'));
-  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
 
   return (
     <article className={styles.container}>
-      <div>
-        <span className={styles.date}>
-          <Calendar /> {formattedDayAndMonth}
-        </span>
+      <H3 className={styles.title}>{event.title}</H3>
 
-        <H3 className={styles.title}>{event.title}</H3>
+      <span className={styles.date}>
+        <Calendar /> {formattedDayAndMonth}
+      </span>
 
-        {event.sponsor && (
-          <p className={styles.sponsor}>
-            Sponsorisé par <strong>{event.sponsor.name}</strong>
-          </p>
-        )}
-
-        <div className={styles.description} style={{ display: isDescriptionExpanded ? 'block' : '-webkit-box' }}>
-          <ReactMarkdown>{event.description}</ReactMarkdown>
-        </div>
-        <button
-          type="button"
-          className={styles.seeMore}
-          onClick={() => setIsDescriptionExpanded((isExpanded) => !isExpanded)}
-        >
-          {isDescriptionExpanded ? 'Voir moins' : 'Voir plus'}
-        </button>
-
-        <ButtonLink href={event.eventUrl} target="_blank" rel="noreferrer noopener">
-          <Meetup /> Participer
-        </ButtonLink>
-      </div>
-
-      <div>
+      <div className={styles.venue}>
         <img src={event.imageUrl} alt={event.title} className={styles.picture} loading="lazy" />
-
-        <a
-          href={`http://maps.google.com/maps?q=loc:${event.venue.lat}+${event.venue.lng}`}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <div className={styles.location}>
-            <Pin />
-
-            {event.sponsor && <p className={styles.sponsor}>{event.sponsor.name}</p>}
-            <p>
-              {event.venue.address} · {event.venue.city}
-            </p>
-          </div>
-        </a>
+        <Location event={event} />
       </div>
+
+      {event.sponsor && (
+        <p className={styles.sponsoredBy}>
+          Sponsorisé par <strong>{event.sponsor.name}</strong>
+        </p>
+      )}
+
+      <Description event={event} />
+
+      <ButtonLink href={event.eventUrl} target="_blank" rel="noreferrer noopener" className={styles.participate}>
+        <Meetup /> Participer
+      </ButtonLink>
 
       <EventMarkup event={event} />
     </article>
