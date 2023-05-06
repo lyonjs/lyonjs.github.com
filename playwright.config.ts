98664dff0773d13210267b46f220b5185ea42ae0
Mathieu Mure <mathieu.mure@gmail.com>
2023-05-06 17:36:32 +0200

    add base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,11 +32,9 @@ export default defineConfig({
   reporter: process.env.CI ? 'html' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:3000',
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
