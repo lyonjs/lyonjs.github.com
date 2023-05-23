9f050e1c734db11b21f7759d86f6cd8f2189d536
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-05-23 11:25:57 +0200

    feat(seo): handle robots and sitemap with next app dir

---
+++ app/sitemap.ts
@@ -0,0 +1,41 @@
+import { MetadataRoute } from 'next';
+import { fetchYearsWithMeetups } from '../modules/meetup/queries/years-with-meetups';
+import { fetchPastEvents } from '../modules/meetup/queries/past-events.api';
+import { slugEventTitle } from '../modules/event/eventSlug';
+
+const BASE_URL = 'https://lyonjs.org';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const years = await fetchYearsWithMeetups();
+  const pastEvents = await fetchPastEvents();
+  const lastModified = new Date();
+
+  return [
+    {
+      url: BASE_URL,
+      lastModified,
+    },
+    {
+      url: `${BASE_URL}/code-de-conduite`,
+      lastModified,
+    },
+    {
+      url: `${BASE_URL}/evenements-precedents`,
+      lastModified,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified,
+    },
+    ...Array.from(years)
+      .reverse()
+      .map((year) => ({
+        url: `${BASE_URL}/evenements-precedents/${year}`,
+        lastModified,
+      })),
+    ...pastEvents.map((event) => ({
+      url: `${BASE_URL}/evenement/${slugEventTitle(event)}`,
+      lastModified,
+    })),
+  ];
+}
