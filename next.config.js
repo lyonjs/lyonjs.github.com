6977775282032a589d32911696be6ee2bc37bc59
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-05-30 11:20:54 +0200

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -12,6 +12,39 @@ const nextConfig = {
     domains: ['pbs.twimg.com'],
     unoptimized: true, // disable as we use export mode (which is not compatible see https://nextjs.org/docs/messages/export-image-api)
   },
+  headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(); battery=(self); geolocation=(); microphone=()',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withMDX(nextConfig);
