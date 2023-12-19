d77b25b1597d4fa9c2863d8068e2db148f5d60f0
Antoine Caron <acaron@scaleway.com>
2023-12-19 22:48:36 +0100

    apply @jsulpis suggestions

--- modules/home/Number.tsx
+++ modules/home/Number.tsx
@@ -5,15 +5,14 @@ import { animate, inView } from 'motion';
 const counter = (elementRef: HTMLDivElement, start: number, end: number) => {
   let current = start;
   let range = end - start;
-  let increment = end > start ? 1 : -1;
-  const step = Math.abs(Math.floor(1000 / range));
-  const timer = setInterval(() => {
-    current += increment;
-    elementRef.textContent = `${current}`;
-    if (current == end) {
-      clearInterval(timer);
-    }
-  }, step);
+  const duration = Math.sqrt(Math.log(range));
+  animate(
+    (progress: number) => {
+      current = Math.floor(start + progress * range);
+      elementRef.textContent = `${current}`;
+    },
+    { duration, easing: 'ease-out' },
+  );
 };
 
 export const Number = ({ value, text }: { value: number; text: string }) => {
@@ -23,11 +22,10 @@ export const Number = ({ value, text }: { value: number; text: string }) => {
   useEffect(() => {
     if (elementRef.current) {
       inView(elementRef.current, ({ target }) => {
-        animate(target, { opacity: 1 }, { delay: 0.2, easing: 'ease-out' }).finished.then(() => {
-          if (counterRef.current) {
-            counter(counterRef.current, Math.floor(value * 0.75), value);
-          }
-        });
+        animate(target, { opacity: 1 }, { delay: 0.2, easing: 'ease-out' });
+        if (counterRef.current) {
+          counter(counterRef.current, Math.floor(value * 0.6), value);
+        }
       });
     }
   }, []);
