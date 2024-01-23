a07932c2ca436261f2d9c94a3efe245c4d276c86
Antoine Caron <acaron@scaleway.com>
2024-01-23 14:39:29 +0100

    fixup test

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle('LyonJS - Communauté lyonnaise des utilisateurs de JavaScript');
+  await expect(page).toHaveTitle('LyonJS - La communauté lyonnaise autour de JavaScript et de son écosystème');
   await expect(page.getByRole('heading', { level: 1 })).toHaveText('MeetupLyonJS');
 });
 
