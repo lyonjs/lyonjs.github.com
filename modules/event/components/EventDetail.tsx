3dd6f08c9b3a81e03e553a5c1c97747a9ebb7b2c
Antoine Caron <acaron@scaleway.com>
2024-04-24 16:45:16 +0200

    feat(event): rework gallery of pictures

--- modules/event/components/EventDetail.tsx
+++ modules/event/components/EventDetail.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { H1 } from '../../atoms/remark/Titles';
 import type { Event } from '../types';
 import styles from './EventDetail.module.css';
@@ -10,6 +11,25 @@ import { Location } from './Location';
 import { Heading } from '../../atoms/heading/Heading';
 
 type Props = { event: Event };
+
+export const PictureComponent = ({ index, image }: { index: number; image: string }) => {
+  return (
+    <li className={styles.li} key={index}>
+      <a href={image} className={styles.imageContainer}>
+        <Image
+          loading="lazy"
+          src={image}
+          sizes="(min-width: 320px) 768w, (min-width: 768px) 1024w, (min-width: 1024px) 1200w"
+          width={224}
+          height={224}
+          alt=""
+          aria-hidden="true"
+        />
+      </a>
+    </li>
+  );
+};
+
 export const EventDetail: React.FC<Props> = ({ event }) => {
   const dateParsed = dayjs(event.dateTime);
   const formattedDayAndMonth = _capitalize(dateParsed.format('dddd D MMMM YYYY à H:mm'));
@@ -20,12 +40,12 @@ export const EventDetail: React.FC<Props> = ({ event }) => {
     images = (
       <section className={styles.imageCollection}>
         <Heading Component="h2">Les images</Heading>
-        <div className={styles.listImages}>
-          {event.photoAlbum.photoSample.map((image) => (
-            <a key={image.source} href={image.source}>
-              <img loading="lazy" src={image.source} alt={event.title} />
-            </a>
-          ))}
+        <div className={styles.lists}>
+          <ul>
+            {event.photoAlbum.photoSample.map((image, index) => (
+              <PictureComponent key={image.source} index={index} image={image.source} />
+            ))}
+          </ul>
         </div>
       </section>
     );
@@ -62,8 +82,8 @@ export const EventDetail: React.FC<Props> = ({ event }) => {
           <img src={event.imageUrl} alt="" className={styles.image} />
           {event.venue && <Location venue={event.venue} />}
         </div>
-        {images}
         {replays}
+        {images}
       </div>
     </>
   );
