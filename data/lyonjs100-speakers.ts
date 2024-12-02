4ea9243a38a0cda65725d0664bb91cd45034a2da
Mathieu Mure <mathieu.mure@disney.com>
2024-12-02 09:45:44 +0100

    feat: update speakers and logo

--- data/lyonjs100-speakers.ts
+++ data/lyonjs100-speakers.ts
@@ -12,11 +12,16 @@ type Speaker = {
 };
 export const speakers: Array<Speaker> = [
   { name: 'Matthieu Lux', avatarUrl: '/speakers/matthieu-lux.webp', social: { twitter: 'swiip', linkedin: 'swiip' } },
-  { name: 'Jérémie Patonnier', avatarUrl: '/speakers/jeremie-patonnier.jpg', social: { twitter: 'jeremiepat' } },
+  // { name: 'Jérémie Patonnier', avatarUrl: '/speakers/jeremie-patonnier.jpg', social: { twitter: 'jeremiepat' } },
+  // {
+  //   name: 'Jonny Burger',
+  //   avatarUrl: '/speakers/jonny-burger.png',
+  //   social: { twitter: 'JNYBGR', linkedin: 'jonny-burger-4115109b' },
+  // },
   {
-    name: 'Jonny Burger',
-    avatarUrl: '/speakers/jonny-burger.png',
-    social: { twitter: 'JNYBGR', linkedin: 'jonny-burger-4115109b' },
+    name: `Bientôt lui 🤫`,
+    avatarUrl: '/speakers/secret.png',
+    social: {},
   },
   {
     name: `Elle aussi !! 🤩`,
