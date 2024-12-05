e3bc2b663637bcd91934c0ac7ee6439d00781986
Mathieu Mure <mathieu.mure@disney.com>
2024-12-05 18:31:23 +0100

    reviews

--- data/lyonjs100-sponsors.ts
+++ data/lyonjs100-sponsors.ts
@@ -1,31 +1,30 @@
 import { Sponsor } from '../modules/event/types';
 import { bedrock, cleverCloud, indy, malt, theodo, wanadev, zenika } from './sponsors';
 
-export const gold: Array<Sponsor> = [
-  malt,
-  {
-    name: 'Fulll',
-    url: 'https://www.fulll.fr/',
-    logo: '/sponsors/fulll.webp',
-  },
-  {
-    name: 'Kolecto',
-    url: 'https://www.kolecto.fr/',
-    logo: '/sponsors/kolecto.svg',
-  },
-  { name: 'Akawaka', url: 'https://www.akawaka.fr/', logo: '/sponsors/akawaka.svg' },
-];
+export const fulll: Sponsor = {
+  name: 'Fulll',
+  url: 'https://www.fulll.fr/',
+  logo: '/sponsors/fulll.webp',
+};
 
-export const silver: Array<Sponsor> = [
-  { name: '365 Talent', url: 'https://www.365talents.com/fr/', logo: '/sponsors/365Talent.svg' },
-  zenika,
-  bedrock,
-  indy,
-  theodo,
-];
+export const kolecto: Sponsor = {
+  name: 'Kolecto',
+  url: 'https://www.kolecto.fr/',
+  logo: '/sponsors/kolecto.svg',
+};
 
-export const bronze = [
-  cleverCloud,
-  wanadev,
-  { name: 'Feelinjob', url: 'https://www.feelinjob.com/', logo: '/sponsors/feelinjob.svg' },
-];
+export const feelinjob = { name: 'Feelinjob', url: 'https://www.feelinjob.com/', logo: '/sponsors/feelinjob.svg' };
+
+export const akawaka: Sponsor = { name: 'Akawaka', url: 'https://www.akawaka.fr/', logo: '/sponsors/akawaka.svg' };
+
+export const _365talents: Sponsor = {
+  name: '365 Talent',
+  url: 'https://www.365talents.com/fr/',
+  logo: '/sponsors/365Talent.svg',
+};
+
+export const gold: Array<Sponsor> = [malt, fulll, kolecto];
+
+export const silver: Array<Sponsor> = [_365talents, zenika, bedrock, indy, akawaka, theodo];
+
+export const bronze = [cleverCloud, wanadev, feelinjob];
