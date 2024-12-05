6a559baa8b9ea5b460b4af98f218ca197ca741a4
Mathieu Mure <mathieu.mure@disney.com>
2024-12-05 18:31:23 +0100

    reviews

--- data/lyonjs100-jobs.ts
+++ data/lyonjs100-jobs.ts
@@ -1,15 +1,9 @@
 import { malt, zenika } from './sponsors';
+import { kolecto, fulll, _365talents } from './lyonjs100-sponsors';
 import { Job } from '../modules/jobs/jobs.types';
 
-export const lyonjs100Jobs: Array<Job> = [
-  {
-    title: 'Développeur Fullstack',
-    url: 'https://www.zenika.com',
-    sponsor: zenika,
-    type: 'CDI',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet error fuga illum perspiciatis possimus quam, quibusdam recusandae. Culpa dolores est facere ipsum minima molestias neque placeat quam quis. Accusantium, architecto.',
-  },
+/** 3 jobs max per gold sponsors */
+const goldSponsorJobs: Array<Job> = [
   {
     title: 'Développeur Frontend',
     url: 'https://www.google.com',
@@ -24,4 +18,54 @@ export const lyonjs100Jobs: Array<Job> = [
     type: 'CDI',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fus',
   },
+  {
+    title: 'Développeur Backend',
+    url: 'https://www.google.com',
+    sponsor: malt,
+    type: 'CDI',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fus',
+  },
+  {
+    title: 'Développeur Backend',
+    url: 'https://www.google.com',
+    sponsor: kolecto,
+    type: 'CDI',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fus',
+  },
+  {
+    title: 'Développeur Backend',
+    url: 'https://www.google.com',
+    sponsor: kolecto,
+    type: 'CDI',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fus',
+  },
+  {
+    title: 'Développeur Backend',
+    url: 'https://www.google.com',
+    sponsor: fulll,
+    type: 'CDI',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fus',
+  },
 ];
+
+/** 1 job max per silver sponsors */
+const silverSponsorJobs: Array<Job> = [
+  {
+    title: 'Développeur Fullstack',
+    url: 'https://www.zenika.com',
+    sponsor: zenika,
+    type: 'CDI',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet error fuga illum perspiciatis possimus quam, quibusdam recusandae. Culpa dolores est facere ipsum minima molestias neque placeat quam quis. Accusantium, architecto.',
+  },
+  {
+    title: 'Développeur Fullstack',
+    url: 'https://www.zenika.com',
+    sponsor: _365talents,
+    type: 'Stage',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet error fuga illum perspiciatis possimus quam, quibusdam recusandae. Culpa dolores est facere ipsum minima molestias neque placeat quam quis. Accusantium, architecto.',
+  },
+];
+
+export const lyonJS100Jobs = [...goldSponsorJobs, ...silverSponsorJobs];
