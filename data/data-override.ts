539203fe7d531d0a7742f26d9ada0ee21006c13b
Antoine Caron <acaron@scaleway.com>
2024-12-15 15:34:26 +0100

    feat: attach sponsor to last minute

--- data/data-override.ts
+++ data/data-override.ts
@@ -1,7 +1,10 @@
 import type { Event } from '../modules/event/types';
-import { bedrock, indy, leWagon, malt, shodo, smile, theodo, wanadev, wildCodeSchool, zenika } from './sponsors';
+import { bedrock, CBTW, indy, leWagon, malt, shodo, smile, theodo, wanadev, wildCodeSchool, zenika } from './sponsors';
 
 export const dataOverride: { [key: string]: Partial<Event> } = {
+  'https://www.meetup.com/lyonjs/events/304691839': {
+    sponsor: CBTW,
+  },
   'https://www.meetup.com/lyonjs/events/304182290': {
     sponsor: theodo,
     talks: [
