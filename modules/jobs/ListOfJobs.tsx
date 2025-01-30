29685e11e086f5a185905d222d380476e4cb74b9
Mathieu Mure <mathieu.mure@disney.com>
2025-01-30 19:26:45 +0100

    feat(lyonjs-100/jobs): add fulll jobs

--- modules/jobs/ListOfJobs.tsx
+++ modules/jobs/ListOfJobs.tsx
@@ -20,11 +20,11 @@ export const ListOfJobs = ({ jobs }: Props) => {
               <div className={styles.header}>
                 <p className={styles.type}>{job.type}</p>
                 <p className={styles.sponsor}>{job.sponsor.name}</p>
-                <img className={styles.logo} src={job.sponsor.logo} alt={job.sponsor.name} />
+                <img className={styles.headerLogo} src={job.sponsor.logo} alt={job.sponsor.name} />
               </div>
               <H2 className={styles.title}>{job.title}</H2>
               <ReactMarkdown className={styles.description}>{job.description}</ReactMarkdown>
-              <img className={styles.logo} src={job.sponsor.logo} alt={job.sponsor.name} />
+              <img className={styles.footerLogo} src={job.sponsor.logo} alt={job.sponsor.name} />
               <ButtonLink href={job.url} target="_blank" rel="noreferrer noopener" className={styles.participate}>
                 Voir l'annonce <span aria-hidden="true">&rarr;</span>
               </ButtonLink>
