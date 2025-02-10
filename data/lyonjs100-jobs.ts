4d828a894eebac732f30fbca472b9c6537f9b4cb
Antoine Caron <acaron@scaleway.com>
2025-02-10 13:54:21 +0100

    feat(lyonjs100): add kolecto job offers

--- data/lyonjs100-jobs.ts
+++ data/lyonjs100-jobs.ts
@@ -13,14 +13,14 @@ const goldSponsorJobs: Array<Job> = [
 
 En tant qu’architecte, tu formes un binôme parfait avec un chef de produit. Tu interviens sur l’ensemble du cycle de vie du produit, de la mise en place de l’architecture initiale, ses évolutions et tout ce qui permet de s’assurer du déploiement continu (pipeline ci/cd). Tu partages ton savoir-faire aux autres membres de l’équipe. Tu sensibilises notamment les développeurs aux pratiques DevOps, tu leur fournis et maintiens un cadre. Tu sais que le meilleur moyen de faire avancer les projets est d’y contribuer, donc tu es à l’aise avec le développement Javascript notamment Node.js, que tu préfères le front et/ou le back.
 
-# Quel sera ton quotidien ?
+## Quel sera ton quotidien ?
 * S’assurer de la conformité et de la cohérence technique
 * Spécification sur des sujets complexes
 * Conseiller son équipe
 * Échanger avec les autres architectes
 * Conseiller sur les aspects de la roadmap
 
-# Quels seront tes avantages ?
+## Quels seront tes avantages ?
 * Workshop toutes les 5 semaines
 * Un accès illimité à la plateforme Udemy pour toujours en apprendre +
 * Du bon café gratuit pour bien travailler
@@ -38,7 +38,7 @@ En tant qu’architecte, tu formes un binôme parfait avec un chef de produit. T
 
 En tant que développeur frontend, tu maîtrises à minima la programmation en JavaScript / React. Tu as même peut-être contribué à améliorer un ou plusieurs projets open source. Au-delà de tous ces concepts purement techniques, tu es sensible à livrer du code qui fonctionne, mais surtout bien écrit afin qu’il soit fiable et aisément maintenable (principe du software craftmanship). Tel un caméléon tu es intéressé(e) par la production d’applications qui résolvent un problème métier !
 
-# Quel sera ton quotidien ?
+## Quel sera ton quotidien ?
 * Standup
 * Conception
 * Développement (code writing)
@@ -46,7 +46,7 @@ En tant que développeur frontend, tu maîtrises à minima la programmation en J
 * Apprentissage continu
 * Résolution de problèmes
 
-# Quels seront tes avantages ?
+## Quels seront tes avantages ?
 * Workshop toutes les 5 semaines
 * Un accès illimité à la plateforme Udemy pour toujours en apprendre +
 * Du bon café gratuit pour bien travailler
@@ -68,7 +68,7 @@ Tout cela te parle ?
 
 👇 Je t’invite à lire la suite 👇
 
-# Quel sera ton quotidien ?
+## Quel sera ton quotidien ?
 * Standup
 * Conception
 * Développement (code writing)
@@ -77,13 +77,85 @@ Tout cela te parle ?
 * Résolution de problèmes
 
 
-# Quels seront tes avantages ?
+## Quels seront tes avantages ?
 * Workshop toutes les 5 semaines
 * Un accès illimité à une plateforme d’e-learning pour toujours en apprendre +
 * Du bon café gratuit pour bien travailler
 * Des corbeilles de fruits pour faire le plein de vitamines
 * Des horaires aménagés et du télétravail jusqu’à 3 jours par semaine
 * Une carte Swile (Ticket restaurant)`,
+  },
+  {
+    title: 'Senior Frontend Software Engineer (H/F/X)',
+    url: 'https://www.linkedin.com/jobs/view/4114822486',
+    sponsor: kolecto,
+    type: 'CDI',
+    description: `
+Kolecto, filiale de Crédit Agricole et de la Fabrique by CA, est une fintech en pleine croissance qui aide les dirigeants de TPE/PME et leurs collaborateurs à reprendre le contrôle de leurs finances et de leur temps.
+
+En tant que Senior Frontend Software Engineer, vous jouerez un rôle essentiel dans la conception et l’amélioration de l’expérience utilisateur de notre plateforme.
+
+## Responsabilités principales :
+* Développer et maintenir des interfaces utilisateur réactives et performantes en utilisant les technologies modernes, tout en suivant les bonnes pratiques de développement (structuration du code, documentation, maintenabilité) ;
+* Collaborer avec les équipes Backend, produit et design pour assurer une expérience utilisateur optimale et cohérente ;
+* Participer activement à l’amélioration de l’architecture frontend et à l'intégration de nouvelles technologies, tout en assurant la qualité du code à travers des revues rigoureuses et des tests automatisés ;
+* Assurer la qualité du code par des revues de code rigoureuses et la mise en place de tests automatisés ;
+* Encadrer et accompagner techniquement les développeur·se·s junior et intermédiaires de l’équipe en les guidant sur les meilleures pratiques de développement frontend ;
+* Effectuer une veille technologique et proposer des améliorations continues pour rester à la pointe des meilleures pratiques.
+
+## Nos talents rejoignent Kolecto pour :
+* Nos équipes douées, passionnées, expérimentées et altruistes, qui évoluent dans une dynamique de croissance très positive.
+* Notre environnement de travail hybride unique, combinant le meilleur de l’environnement stimulant de la startup et la force de frappe d’un grand groupe bancaire leader.
+* Notre politique de télétravail flexible, qui permet jusqu'à 3 jours de télétravail par semaine.
+* Nos événements internes fédérateurs : deux séminaires par an, talks inspirants, afterworks, etc.
+`,
+  },
+  {
+    title: 'Frontend Software Engineer (H/F/X)',
+    url: 'https://www.linkedin.com/jobs/view/4114829117',
+    sponsor: kolecto,
+    type: 'CDI',
+    description: `
+Kolecto, filiale de Crédit Agricole et de la Fabrique by CA, est une fintech en pleine croissance qui aide les dirigeants de TPE/PME et leurs collaborateurs à reprendre le contrôle de leurs finances et de leur temps.
+
+En tant que Frontend Software Engineer, vous contribuez activement à la conception et à l'optimisation de l'expérience utilisateur de notre plateforme.
+
+## Responsabilités principales :
+* Développer et maintenir des interfaces utilisateur réactives et performantes en utilisant les technologies modernes, tout en suivant les bonnes pratiques de développement (structuration du code, documentation, maintenabilité) ;
+* Collaborer avec les équipes de développement back end et produit pour assurer une intégration fluide ;
+* Participer à l’amélioration continue de l’architecture front end, en veillant à la qualité du code, la performance et l’expérience utilisateur ;
+* Contribuer à l’évolution de notre design system et assurer sa cohérence avec l’ensemble de l’application ;
+* Assurer une veille technologique afin de proposer des solutions innovantes et maintenir la compétitivité de notre produit.
+
+## Nos talents rejoignent Kolecto pour :
+* Nos équipes douées, passionnées, expérimentées et altruistes, qui évoluent dans une dynamique de croissance très positive.
+* Notre environnement de travail hybride unique, combinant le meilleur de l’environnement stimulant de la startup et la force de frappe d’un grand groupe bancaire leader.
+* Notre politique de télétravail flexible, qui permet jusqu'à 3 jours de télétravail par semaine.
+* Nos événements internes fédérateurs : deux séminaires par an, talks inspirants, afterworks, etc.
+    `,
+  },
+  {
+    title: 'Back-End Software Engineer (H/F/X)',
+    url: 'https://www.linkedin.com/jobs/view/4114827201',
+    sponsor: kolecto,
+    type: 'CDI',
+    description: `
+Kolecto, filiale de Crédit Agricole et de la Fabrique by CA, est une fintech en pleine croissance qui aide les dirigeants de TPE/PME et leurs collaborateurs à reprendre le contrôle de leurs finances et de leur temps.
+
+En tant que Backend Software Engineer, vous contribuerez activement au développement et à l'évolution technique de notre solution en assurant toutes les étapes du processus de développement, de la conception au déploiement, tout en collaborant avec les équipes tech et produit.
+
+## Responsabilités principales :
+* Assurer toutes les étapes du développement du produit (conception, développement, test/qualité, déploiement) ;
+* Participer à l’évolution de notre stack technique (architecture, librairies, tests, performance, monitoring, etc.) ;
+* Travailler en collaboration avec les autres membres de l’équipe technique, ainsi que l’équipe produit (méthodes agiles) ;
+* Assurer une veille technologique adaptée aux besoins de l’entreprise.
+
+## Nos talents rejoignent Kolecto pour :
+* Nos équipes douées, passionnées, expérimentées et altruistes, qui évoluent dans une dynamique de croissance très positive.
+* Notre environnement de travail hybride unique, combinant le meilleur de l’environnement stimulant de la startup et la force de frappe d’un grand groupe bancaire leader.
+* Notre politique de télétravail flexible, qui permet jusqu'à 3 jours de télétravail par semaine.
+* Nos événements internes fédérateurs : deux séminaires par an, talks inspirants, afterworks, etc.
+    `,
   },
 ];
 
