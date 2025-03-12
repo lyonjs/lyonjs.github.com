86c1adcd2378913dcb81a711d60e52121c64e78b
Antoine Caron <acaron@scaleway.com>
2025-03-12 22:57:53 +0100

    fix: use async way of fetching params

--- app/evenements-precedents/[year]/page.tsx
+++ app/evenements-precedents/[year]/page.tsx
@@ -1,23 +1,24 @@
 import { H1 } from '../../../modules/atoms/remark/Titles';
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Metadata } from 'next';
 import { PastEvents } from './pastEvents';
-import { Loader } from './loader';
 
 export const revalidate = 3600;
+const DEFAULT_YEAR = `${new Date().getFullYear()}`;
 
-export default async function PastEventsPage({ params: { year } }: { params: { year: string } }) {
+export default async function PastEventsPage({ params }: { params: Promise<{ year?: string }> }) {
+  const { year = DEFAULT_YEAR } = await params;
+
   return (
     <main>
       <H1>Évènements précédents</H1>
-      <Suspense fallback={<Loader />}>
-        <PastEvents year={year}></PastEvents>
-      </Suspense>
+      <PastEvents year={year}></PastEvents>
     </main>
   );
 }
 
-export async function generateMetadata({ params: { year } }: { params: { year: string } }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: Promise<{ year?: string }> }): Promise<Metadata> {
+  const { year = DEFAULT_YEAR } = await params;
   const title = `LyonJS | Évènements ${year}`;
   const description = `Liste des évènements LyonJS de l'année ${year}, meetup, apéros, conférences et rassemblement, retrouver tous les liens depuis notre création.`;
 
