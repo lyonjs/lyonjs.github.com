036b02b9004ddd350e04ddc1633253865e197709
Antoine Caron <Slashgear@users.noreply.github.com>
2025-09-29 20:03:06 +0200

    feat(sponsor): some sponsor cleanup (#547)

--- modules/footer/Footer.tsx
+++ modules/footer/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './Footer.module.css';
 
 export const Footer = () => {
@@ -12,14 +11,6 @@ export const Footer = () => {
         aria-label="Compte Twitter du LyonJS"
       >
         @LyonJS
-      </a>
-      <a href="https://vercel.com?utm_source=lyonjs&utm_campaign=oss">
-        <Image
-          alt="Vercel"
-          width="200"
-          height="100"
-          src="https://images.ctfassets.net/e5382hct74si/78Olo8EZRdUlcDUFQvnzG7/fa4cdb6dc04c40fceac194134788a0e2/1618983297-powered-by-vercel.svg"
-        />
       </a>
     </footer>
   );
